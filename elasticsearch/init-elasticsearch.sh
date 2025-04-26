cf8638eff20600c6c7dfc2e991b79661c2dae2b0
Guilherme Savio <guilhermesavio02@hotmail.com>
2025-04-26 17:31:11 -0300

    fix: :bug: Minimal fixes

--- elasticsearch/init-elasticsearch.sh
+++ elasticsearch/init-elasticsearch.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 set -e
 
-echo "⏳ Esperando Elasticsearch subir..."
 until curl -s http://localhost:9200 >/dev/null; do
   sleep 2
 done
-
-echo "✅ Elasticsearch disponível. Criando índice 'products'..."
 
 curl -X PUT "http://localhost:9200/products" \
   -H 'Content-Type: application/json' \
