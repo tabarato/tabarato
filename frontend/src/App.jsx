d4221b89294e421bf46c4c204b929ae169f2eb66
Guilherme Savio <guilherme.savio@galax-ia.com>
2025-05-13 21:17:48 -0300

    feat: :sparkles: Starting React + Daisy + Capacitor

---
+++ frontend/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="card bg-base-100 w-96 shadow-sm">
+        <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            alt="Shoes" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Card Title</h2>
+          <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+      <div className="mockup-phone">
+        <div className="mockup-phone-camera"></div>
+        <div className="mockup-phone-display text-white grid place-content-center">It's Glowtime.</div>
+      </div>
+
+    </>
+  )
+}
+
+export default App
