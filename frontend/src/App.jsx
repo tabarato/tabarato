5094e2bed38569e75e20cbc8cdaba2822c0453de
Guilherme Savio <guilherme.savio@galax-ia.com>
2025-05-14 20:55:16 -0300

    feat: :sparkles: Adding frontend main structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,33 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import ResultPage from './pages/Result';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div className="card bg-base-100 w-96 shadow-sm">
-        <figure>
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-            alt="Shoes" />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">Card Title</h2>
-          <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
-          </div>
-        </div>
-      </div>
-      <div className="mockup-phone">
-        <div className="mockup-phone-camera"></div>
-        <div className="mockup-phone-display text-white grid place-content-center">It's Glowtime.</div>
-      </div>
-
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/result" element={<ResultPage />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
